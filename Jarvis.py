30a8471784d64ca977f567d1c69633e8666fbf9a
Endri Dibra <104068619+EndriDibra@users.noreply.github.com>
2022-12-14 02:13:18 +0200

    Update Jarvis.py

--- Jarvis.py
+++ Jarvis.py
@@ -1,5 +1,23 @@
 # Author: Endri Dibra
 
+"""
+The user can ask some predefined questions JARVIS.
+
+Questions / Keywords:
+1. "how are you"
+2. "are you there?"
+3. "time?"
+4. "social media"
+5. "weather"
+6. "exit"
+
+Or you can ask in a different way your questions, but you should contain inside the phrase the above keywords!
+For Example:
+What time is it? , Go for social media , You can exit now.
+
+You can also copy-paste the code on your IDE and change the keywords [Put your own commands] as well.
+"""
+
 # importing the required libraries
 import time
 import datetime
